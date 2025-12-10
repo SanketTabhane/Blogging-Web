b3274bbfe4a35af11eb2ec99bc439faa9071f118
Sanket <sankettbhane5@gmail.com>
2025-12-11 01:19:38 +0530

    Posts by Category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Blog, Category
 
-# Create your views here.
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    # for custom page we use try and except for that we need to false debug in settings and put * value 
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+    # except:
+    #     return redirect('home')
+    # for simple 404 error page 
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'posts':posts,
+        'category':category,
+    }
+    return render(request, 'posts_by_category.html', context)
